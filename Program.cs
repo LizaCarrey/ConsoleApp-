4bf9053c92caee48cf809ff77d960ba0c1140a50
Елизавета <@DESKTOP-1VHS314>
2024-01-23 21:49:16 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        //{
+        //    //Задание 1. Вывести на экран в столбик первые 10 натуральных чисел
+        //    //(циклы с условием, цикл с параметром)
+
+        //    //с условием:
+        //    int x = 1;
+        //    while (x<=10)
+        //    {
+        //        Console.WriteLine(x);
+        //        x++;
+        //    }
+        //    //с параметром:
+        //    for (int x = 1 ; x <= 10 ; x++) 
+        //    {
+        //        Console.WriteLine(x);
+        //    }
+
+
+        //    //Задание 2.Вывести на экран целые числа из интервала от 1 до n
+        //    //(цикл с параметром)
+        //    Console.WriteLine("введите число - ");
+        //    int n = int.Parse(Console.ReadLine());
+        //    for (int x =1 ; x <= 10 ; x++) 
+        //    {
+        //        Console.WriteLine(x);
+        //    }
+
+        //    //Задание 3. Вывести в столбик 5 раз слово «Привет!» (циклы с условием)
+        //    int x = 1;
+        //    while (x<=5) 
+        //    {
+        //        Console.WriteLine("привет!");
+        //        x++;
+        //    }
+
+        //    //Задание 4. С использованием цветового оформления консоли:
+
+        //    //а) вывести на экран горизонтальную строку из 18 символов;
+        //    Console.ForegroundColor = ConsoleColor.Magenta;
+        //    for (int i= 1 ; i <= 10 ; i++)
+        //    {
+        //        Console.Write('.');
+        //    }
+
+
+        //    //б) вывести на экран вертикальную строку из 25 символов.
+        //    Console.ForegroundColor = ConsoleColor.Blue;
+        //    for (int i = 1; i <= 25; i++)
+        //    {
+        //        Console.Write('.');
+        //    }
+            Console.ReadKey();
+        }
+    }
+}
